0027b95fec8837deaa0c2c371014059fff99ab0d
brent <brent@DESKTOP-FCLKQ8T>
2020-10-05 18:21:27 -0700

    Finished AddingMachine

--- 360Assignment01/src/cse360assignment02/AddingMachine.java
+++ 360Assignment01/src/cse360assignment02/AddingMachine.java
@@ -1,5 +1,32 @@
 package cse360assignment02;
 
 public class AddingMachine {
+  private int total;
+  private String history;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) {
+	  total += value;
+	  history += " + " + value;
+  }
 
+  public void subtract (int value) {
+	  total -= value;
+	  history += " - " + value;
+  }
+
+  public String toString () {
+	  return history;
+  }
+
+  public void clear() {
+	  System.gc();
+  }
 }
